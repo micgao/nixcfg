3ec27ba60787ed2319bad22b01a1e7ca834ee553
Michael G <micgao@pm.me>
2023-08-23 12:14:30 -0400

    updates

--- home-manager/waybar/default.nix
+++ home-manager/waybar/default.nix
@@ -1,6 +1,20 @@
 { pkgs, ... }: {
   programs.waybar = {
     enable = true;
+    package = pkgs.waybar.override {
+      cavaSupport = true;
+      hyprlandSupport = true;
+      inputSupport = true;
+      jackSupport = true;
+      mpdSupport = true;
+      mprisSupport = true;
+      nlSupport = true;
+      pulseSupport = true;
+      sndioSupport = true;
+      traySupport = true;
+      udevSupport = true;
+      wireplumberSupport = true;
+    };
   };
   xdg.configFile."waybar/config".source = ./config;
   xdg.configFile."waybar/style.css".source = ./style.css;
