74e33aa05de44cfeb2f59976f378730bca6a4c12
Michael G <micgao@pm.me>
2023-10-02 08:02:02 -0400

    update

--- flake.nix
+++ flake.nix
@@ -38,7 +38,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, nix-index-database, hyprland, hyprpicker, hyprpaper, helix, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
 
   let
     inherit (self) outputs;
