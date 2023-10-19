22c2662df2bc14b6f92bdb1bccb9494364573496
Michael G <micgao@pm.me>
2023-10-19 09:47:06 -0400

    Updates

--- home-manager/nix-your-shell.nix
+++ home-manager/nix-your-shell.nix
@@ -3,7 +3,7 @@
     inputs.nix-your-shell.overlays.default
   ];
   home = {
-    packages = pkgs.nix-your-shell;
+    packages = [ pkgs.nix-your-shell ];
   };
   home.file."${config.xdg.configHome}/nushell/nix-your-shell.nu".source = pkgs.nix-your-shell.generate-config "nu";
 }
