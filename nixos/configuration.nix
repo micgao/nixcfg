b0a086452905a7cf6a6d8aa616d54bae930b034c
Michael G <micgao@pm.me>
2023-11-05 09:55:10 -0500

    update

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -80,9 +80,6 @@
     systemPackages = with pkgs; [
       btrfs-progs
       appimage-run
-      nixgl.auto.nixGLDefault
-      nixgl.auto.nixGLNvidia
-      nixgl.auto.nixVulkanNvidia
     ];
     variables = {
       EDITOR = "nvim";
