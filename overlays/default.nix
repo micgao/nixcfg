f3ba0d0bad423f84547b6b4d7f79d40db1d68e14
Michael G <micgao@pm.me>
2023-11-13 13:15:11 -0500

    update

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,5 +2,12 @@
 {
   additions = final: _prev: import ../pkgs { pkgs = final; };
   modifications = final: prev: {
+    nushell = prev.nushell.overrideAttrs (oldAttrs: {
+      buildFeatures = [
+      	"extra"
+	      "dataframe"
+	      "plugin"
+      ];
+    });
   };
 }
