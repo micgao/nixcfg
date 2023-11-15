4c0d6f5bdd1e94a1e242de31b94ae85869419138
Michael G <micgao@pm.me>
2023-11-15 13:42:44 -0500

    update

--- home-manager/neovim.nix
+++ home-manager/neovim.nix
@@ -1,11 +1,4 @@
 { inputs, pkgs, ... }:
-let
-  nvim-treesitter = pkgs.vimPlugins.nvim-treesitter.withAllGrammars;
-  treesitter-parsers-deps = pkgs.symlinkJoin {
-    name = "treesitter-parsers-deps";
-    paths = nvim-treesitter.dependencies;
-  };
-in
 {
   nixpkgs = {
     overlays = [
@@ -18,30 +11,13 @@ in
     package = pkgs.neovim.override {
       libvterm-neovim = inputs.nixpkgs-staging.legacyPackages.x86_64-linux.libvterm-neovim;
     };
-    plugins = with pkgs.vimPlugins; [
-      nvim-treesitter
-    ];
     extraPackages = with pkgs; [
       gcc
       clang
       gnumake
-      cmake
-    ];
-    extraLuaPackages = luaPkgs: with luaPkgs; [
-      luarocks
     ];
     viAlias = true;
     vimAlias = true;
     vimdiffAlias = true;
-  };
-  xdg.configFile = {
-    "nvim/init.lua".text = ''
-      vim.opt.runtimepath:append("${nvim-treesitter}")
-      vim.opt.runtimepath:append("${treesitter-parsers-deps}")
-      require("config.options")
-      require("config.lazy")
-      require("config.autocmds")
-      require("config.keymap")
-    '';
   };
 }
