9859cf90d286d39a1de811595fba0b8550fcae3c
Michael G <micgao@pm.me>
2023-11-21 13:19:24 -0500

    update

--- home-manager/anyrun.nix
+++ home-manager/anyrun.nix
@@ -6,5 +6,15 @@
   programs.anyrun = {
     enable = true;
     package = inputs.anyrun.packages.${pkgs.system}.anyrun-with-all-plugins;
+    plugins = with inputs.anyrun.packages.${pkgs.system}; [
+      applications
+      kidex
+      randr
+      rink
+      shell
+      stdin
+      symbols
+      websearch
+    ];
   };
 }
