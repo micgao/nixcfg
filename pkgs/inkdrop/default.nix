8fb66456d0355d38f1d6b13de3d1a8f8624d6513
Michael G <micgao@pm.me>
2023-12-13 12:36:34 -0500

    update

--- pkgs/inkdrop/default.nix
+++ pkgs/inkdrop/default.nix
@@ -1,10 +1,6 @@
 { stdenv, dpkg, glibc, fetchurl, gcc-unwrapped, autoPatchelfHook }:
 let
-
-  src = fetchurl {
-    url = "https://api.inkdrop.app/download/linux/deb";
-    hash = "sha256-85c6d5eca7799c929bf81fa545ae49d42d027360fddd68746513508e0c1c4f3e";
-  };
+  src = ./inkdrop_5.6.2_amd64.deb;
 
 in stdenv.mkDerivation {
   name = "inkdrop";
