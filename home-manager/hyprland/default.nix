d2ef8a6e706872c03ae49a21d3ef34fe547d6d9d
Michael G <micgao@pm.me>
2024-01-28 04:26:43 -0500

    update

--- home-manager/hyprland/default.nix
+++ home-manager/hyprland/default.nix
@@ -63,7 +63,7 @@ in {
       # env=GBM_BACKEND,nvidia-drm
       exec-once=${pkgs.libsForQt5.polkit-kde-agent}/libexec/polkit-kde-authentication-agent-1
       exec-once=hyprpaper
-      exec-once=waybar
+      # exec-once=waybar
       exec-once=hyprctl setcursor ${pointer.name} ${toString pointer.size}
       exec-once=[workspace 1 silent] wezterm
       exec-once=[workspace 2 silent] librewolf
