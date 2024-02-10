ecbbd4f83aec9fc5261dc15cb22272de6a2b7819
Michael G <micgao@pm.me>
2024-02-10 08:07:43 -0500

    update

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,7 +2,6 @@
   alacritty = pkgs.callPackage ./alacritty { };
   feather-wallet = pkgs.callPackage ./feather-wallet { };
   qobuz-dl = pkgs.callPackage ./qobuz-dl { };
-  # wezterm = pkgs.callPackage ./wezterm { };
   iosevka-ss04 = pkgs.callPackage ./iosevka { };
   # iosevka-murpins = pkgs.callPackage ./iosevka-murpins { };
   inter = pkgs.callPackage ./inter { };
