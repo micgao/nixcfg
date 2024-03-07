41715f4c2bdcfa236b0a43f0c035c2787976892d
Michael G <micgao@pm.me>
2024-03-06 22:49:24 -0500

    update

---
+++ nixos/cachix/bloop.nix
@@ -0,0 +1,12 @@
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://bloopai.cachix.org"
+      ];
+      trusted-public-keys = [
+        "bloopai.cachix.org-1:uSHFor+Jd3znikUnLc58xnHBXTcuIBSjdJxV5rLIMJU="
+      ];
+    };
+  };
+}
