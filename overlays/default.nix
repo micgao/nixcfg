428a0f6ca24e59151d8b8aa027b1fbaffc061a8a
Michael G <micgao@pm.me>
2024-03-08 11:14:00 -0500

    update

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,7 +2,7 @@
   additions = final: _prev: import ../pkgs { pkgs = final; };
   modifications = final: prev: {
     nushell = prev.nushell.overrideAttrs (oldAttrs: {
-      cargoBuildFeatures = [
+      buildFeatures = [
         "dataframe"
       ];
     });
