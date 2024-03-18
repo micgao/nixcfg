57c1d59a986d89376a70019a7ad8624e4c79f106
Michael G <micgao@pm.me>
2024-03-18 17:45:56 -0400

    update

--- home-manager/ripgrep.nix
+++ home-manager/ripgrep.nix
@@ -2,7 +2,13 @@
 {
   programs.ripgrep = {
     enable = true;
-    package = pkgs.ripgrep.override { withPCRE2 = true; };
+    package = pkgs.ripgrep.override {
+      withPCRE2 = true;
+      buildFeatures = [
+        "simd-accel"
+        "avx-accel"
+      ];
+    };
     arguments = [ "--max-columns-preview" "--colors=line:style:bold" ];
   };
 }
