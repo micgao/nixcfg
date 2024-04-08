51c860eb3a3e1a069e1ef80a6cfffbe6b8140eed
Michael G <micgao@pm.me>
2024-04-08 16:20:48 -0400

    update

--- home-manager/packages.nix
+++ home-manager/packages.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ pkgs, lib, inputs, stdenv, ... }: {
 
   nixpkgs = {
     config = {
@@ -14,7 +14,19 @@
     packages = with pkgs; [
       firefox-devedition
       mullvad-browser
-      warp-terminal
+      warp-terminal.overrideAttrs (old: rec {
+        pname = "warp-terminal";
+        version = "0.2024.04.02.08.02.stable_01";
+        src = pkgs.fetchurl {
+          url = "https://releases.warp.dev/stable/v${version}/warp-terminal-v${version}-1-x86_64.pkg.tar.zst";
+          sha256 = "sha256-xnXRg23AdfCk2TKBr+PZ3wDYqTN4+8wLSodWpmh3D/Y=";
+        };
+        nativeBuildInputs = old.nativeBuildInputs ++ [pkgs.makeWrapper];
+        postInstall = ''
+          wrapProgram $out/bin/warp-terminal --set WARP_ENABLE_WAYLAND 1 \
+            --prefix LD_LIBRARY_PATH : ${pkgs.wayland}/lib
+        '';
+      })
       rustup
       distrobox
       comma
