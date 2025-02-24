77b754854f74922e8d61285b847ec24e390af22b
Michael G <micgao@pm.me>
2025-02-24 13:50:03 -0500

    update

--- home-manager/vscodium.nix
+++ home-manager/vscodium.nix
@@ -4,28 +4,30 @@
     enable = true;
     package = pkgs.vscodium-fhs;
     mutableExtensionsDir = true;
-    extensions = with pkgs; [
-      vscode-extensions.mkhl.direnv
-      vscode-extensions.asvetliakov.vscode-neovim
-      # vscode-extensions.rust-lang.rust-analyzer
-      # vscode-extensions.vadimcn.vscode-lldb
-    ];
-    userSettings = {
-      "update.mode" = "none";
-      "workbench.colorTheme" = "Sinequanone Noir";
-      "workbench.startupEditor" = "none";
-      "workbench.activityBar.location" = "hidden";
-      "window.titleBarStyle" = "custom";
-      "editor.fontFamily" = "Iosevka SS04";
-      "editor.cursorBlinking" = "solid";
-      "editor.cursorSmoothCaretAnimation" = "on";
-      "diffEditor.codeLens" = true;
-      "diffEditor.diffAlgorithm" = "advanced";
-      "vscode-neovim.neovimClean" = true;
-      "rust-analyzer.diagnostics.experimental.enable" = true;
-      "rust-analyzer.restartServerOnConfigChange" = true;
-      "extensions.experimental.affinity" = {
-        "asvetliakov.vscode-neovim" = 1;
+    profiles = {
+      default = {
+        extensions = with pkgs; [
+          vscode-extensions.mkhl.direnv
+          vscode-extensions.asvetliakov.vscode-neovim
+          # vscode-extensions.rust-lang.rust-analyzer
+          # vscode-extensions.vadimcn.vscode-lldb
+        ];
+        userSettings = {
+          "update.mode" = "none";
+          "workbench.colorTheme" = "Sinequanone Noir";
+          "workbench.startupEditor" = "none";
+          "workbench.activityBar.location" = "hidden";
+          "window.titleBarStyle" = "custom";
+          "editor.fontFamily" = "Iosevka SS04";
+          "editor.cursorBlinking" = "solid";
+          "editor.cursorSmoothCaretAnimation" = "on";
+          "diffEditor.codeLens" = true;
+          "diffEditor.diffAlgorithm" = "advanced";
+          "vscode-neovim.neovimClean" = true;
+          "extensions.experimental.affinity" = {
+            "asvetliakov.vscode-neovim" = 1;
+          };
+        };
       };
     };
   };
