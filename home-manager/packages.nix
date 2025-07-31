778c566901605998d87c67fe98c7cde924f3ae2e
Michael G <micgao@pm.me>
2025-07-31 14:47:07 -0400

    update

--- home-manager/packages.nix
+++ home-manager/packages.nix
@@ -44,7 +44,6 @@
       pavucontrol
       obsidian
       jetbrains-toolbox
-      thunderbird
       feather
       (wine.override { wineBuild = "wineWow"; wineRelease = "staging"; fontconfigSupport = true; alsaSupport = true; gtkSupport = true; openglSupport = true; dbusSupport = true; openclSupport = true; cursesSupport = true; vaSupport = true; pulseaudioSupport = true; udevSupport = true; vulkanSupport = true; sdlSupport = true; usbSupport = true; waylandSupport = true; embedInstallers = true; })
       qobuz-dl
