90dde3219e208b34d1b0c4748a03fb81f154426b
Michael G <micgao@pm.me>
2025-11-15 09:30:38 -0500

    update

--- home-manager/browsers.nix
+++ home-manager/browsers.nix
@@ -3,11 +3,8 @@
     enable = true;
     package = inputs.firefox-nightly.packages.${pkgs.system}.firefox-nightly-bin;
   };
-  # programs.librewolf = {
-  #   enable = true;
-  # };
-  # programs.chromium = {
-  #   enable = true;
-  #   package = pkgs.ungoogled-chromium;
-  # };
+  programs.chromium = {
+    enable = true;
+    package = pkgs.ungoogled-chromium;
+  };
 }
