45969bc17697451049bfa2675891524068408e1f
Michael G <micgao@pm.me>
2026-04-05 07:36:55 -0400

    update

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -14,7 +14,7 @@
     };
     tmp.cleanOnBoot = true;
     consoleLogLevel = 0;
-    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackages;
     kernelParams = [
       "quiet"
       "rd.systemd.show_status=false"
